e21a06d87d516d937af18b0fb4aa6afc0f2b399c
AileOrgla <aileorgla@gmail.com>
2014-09-16 09:01:24 +0300

    Added index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+   <head>
+   <title>Harjutus4</title>
+	 <meta http-equiv="Content-Type" content="text/html;
+   cahrset=utf-8">
+   <title>PHP põhitõed</title>
+	 </head>
+   <body>
+      <h2>Arvutamine murd- ja täisarvudega</h2>
+        <?php 
+        echo $float = 3.14; 
+        echo "<br>";
+        echo $float + 7;
+        echo "<br>";
+        echo 4/3;
+
+        ?> 
+
+
+   </body>
+
+</html>
